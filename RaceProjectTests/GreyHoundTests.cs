67f58decaccbddf34aa79642d75678c9f96ecdb1
Rohitkumar033 <khatrirohit1698@gmail.com>
2019-09-09 00:47:17 +1200

    unit testing

---
+++ RaceProjectTests/GreyHoundTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RaceProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceProject.Tests
+{
+    [TestClass()]
+    public class GreyHoundTests
+    {
+        [TestMethod()]
+        public void RunTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TakeStartingPositionTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ RaceProjectTests/GuyTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RaceProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceProject.Tests
+{
+    [TestClass()]
+    public class GuyTests
+    {
+        [TestMethod()]
+        public void ClearBetTest()
+        {
+            Guy frm = new Guy();
+            frm.ClearBet();
+            Assert.Fail();
+        }
+    }
+}
